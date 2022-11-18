39e87143918e6a0d5e443b39a9ac56ec544d9482
Brian <brian.k.fairbanks@gmail.com>
2022-11-18 14:25:34 -0600

    Adding to caldendar hash table, and outputting text calendar + rejected requests

--- loadFile.py
+++ loadFile.py
@@ -1,13 +1,16 @@
 import csv
 from datetime import datetime
+import itertools
 
 
 class FFighter:
     def __init__(self, fname, lname, hire_date, picks):
         self.fname = fname
         self.lname = lname
+        self.name = f"{lname}, {fname[0]}"
         self.hire_date = hire_date
         self.picks = picks
+        self.picksDetermination = []
         self.priority = None    # default seniority
         self.priorityModifier = 0  # temporary modifier to account for wonky logic
 
@@ -21,18 +24,75 @@ class FFighter:
 
 def setPriorities(arr):
     """Take an array of FFighters.
-        Orders by seniority, and sets the priority flag for each ffighter. 
+        Orders by seniority, and sets the priority flag for each ffighter.
         Returns the sorted array of FFighters"""
     arr.sort(key=lambda x: x.hire_date)
 
     # strange that n, firefighter doesnt seem to work...  implementing manual indexing methods...
-    n = 1
-    for ffighter in arr:
+    for n, ffighter in enumerate(arr):
         # print(f"{n} {ffighter.hire_date}")
         # store this seniority order as priority
         ffighter.priority = n
-        n += 1
     return arr
+
+
+def recheckPriorities(arr):
+    arr.sort(key=lambda x: x.priority-x.priorityModifier)
+    return arr
+
+
+def clearPriorityModifiers(arr):
+    for ffighter in arr:
+        ffighter.priorityModifier = 0
+
+
+def printDictionary(cal):
+    keylist = sorted(cal.keys())
+
+    for key in keylist:
+        print(f"{key}:\n   {cal[key]}\n")
+
+
+def toCalendar(ffighters):
+    calendar = {}
+    rejected = {}
+
+    for n in range(1, 18):
+        # -- every 2 cycles, readdress priority list
+        if (n % 2 == 1):
+            recheckPriorities(ffighters)
+            clearPriorityModifiers(ffighters)
+            # for ffighter in ffighters:
+            #     print(ffighter.hire_date, end=" ")
+            # print("\n\n")
+
+        # -- then assign time off requests
+        for ffighter in ffighters:
+            # print(f"{n} : {calendar} \n")
+            current_pick = ffighter.picks[n]
+
+            if current_pick is None:
+                continue
+
+            # add new date if needed
+            if current_pick not in calendar.keys():
+                calendar[current_pick] = [ffighter.name]
+                continue
+
+            # add to date if possible
+            if len(calendar[current_pick]) < 5:
+                calendar[current_pick].append(ffighter.name)
+                continue
+
+            # mark date as rejected - move up ffighter to top of next priority list
+            if ffighter.name not in rejected.keys():
+                rejected[ffighter.name] = [current_pick]
+            else:
+                rejected[ffighter.name].append(current_pick)
+            ffighter.priorityModifier = 50
+        #
+    #
+    return {"calendar": calendar, "rejected": rejected}
 
 
 # ###########################################################################################################
@@ -71,7 +131,6 @@ def getData(filename):
             ]
 
             for n, date in enumerate(picks):
-                print(n)
                 try:
                     picks[n] = datetime.strptime(date, '%m/%d/%Y').date()
                 except:
@@ -92,8 +151,11 @@ def main():
     # for ffighter in ffighters:
     #     print(ffighter)
     ffighters = setPriorities(getData('testForms.csv'))
-    for ffighter in ffighters:
-        print(ffighter)
+    # for ffighter in ffighters:
+    #     print(ffighter)
+    results = toCalendar(ffighters)
+    printDictionary(results['calendar'])
+    print(results['rejected'])
 
 
 if __name__ == '__main__':
