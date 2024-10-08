ed0ed9b668496c704db7d56be46ab800ad29b97d
Brian <brian.k.fairbanks@gmail.com>
2024-10-08 16:41:42 -0500

    renamed logger to eliminate some confusion

---
+++ setup_logging.py
@@ -0,0 +1,23 @@
+# logging_setup.py
+import logging
+from os import makedirs, path
+
+def setup_logging(filename="default.log", base="..\\logs\\", debug=False):
+    """Setup logging configuration."""
+    if debug:
+        loglevel=logging.DEBUG
+    else:
+        loglevel=logging.INFO
+    full_log_path = path.abspath(path.join(base, filename))
+    if not logging.getLogger().hasHandlers():  # Check if handlers already exist
+        makedirs(base, exist_ok=True)
+        logging.basicConfig(
+            level=loglevel,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+            handlers=[
+                logging.FileHandler(full_log_path),
+                logging.StreamHandler(),  # Optionally add console output
+            ],
+        )
+    logging.info(f"Logging initialized: {full_log_path}")
+    return logging.getLogger(filename)
